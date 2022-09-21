0c153e2a0aae7397e0081eb403f45978101c53b5
kbaitubayev <77764770+kbaitubayev@users.noreply.github.com>
2022-09-21 18:56:34 -0400

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -3,18 +3,23 @@ public class Assignment1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int i, j, a, x=0, y;
-		for(i=1; i<9; i++) {
-			for(a=8; a>i; a--) {
+		
+		//
+		final int ROWNUM = 8; // constantvariable for number of rows
+		int row, column, emptySpace, ascNum=0, descNum;
+		
+		
+		for(row=1; row<ROWNUM+1; row++) {   // for loop to iterate rows
+			for(emptySpace=ROWNUM; emptySpace>row; emptySpace--) { // for loop to place spaces
 			System.out.printf("%5s", " ");
 			}
-			for(j=0; j<i; j++) {
-				x=1<<j;
-			System.out.printf("%5s",x);
+			for(column=0; column<row; column++) {  // for loop for ascending part
+				ascNum=1<<column;
+			System.out.printf("%5s",ascNum);
 			}
-			for(j=1; j<i; j++) {
-				y=x>>j;
-			System.out.printf("%5s",y);
+			for(column=1; column<row; column++) {  // for loop for descending part
+				descNum=ascNum>>column;
+			System.out.printf("%5s",descNum);
 			}
 			System.out.println();
 			
